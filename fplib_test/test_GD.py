4b2b6abfa3ff83cbbd88d940e69b082c05e06ea6
Tack-Tau <taoshuo950111@gmail.com>
2021-01-14 16:37:14 -0500

    Update on test_GD.py

--- fplib_test/test_GD.py
+++ fplib_test/test_GD.py
@@ -25,17 +25,17 @@ def readvasp(vp):
     #rxyz = pos
     return lat, rxyz, types
 
-def test(v1, v2):
+def test1(v1, v2):
     ntyp = 1
-    natx = 300
+    nx = 300
     lmax = 0
     cutoff = 6.5
-    znucl = np.array([26], int)
+    znucl = np.array([3], int)
     lat1, rxyz1, types = readvasp(v1)
     lat2, rxyz2, types = readvasp(v2)
     contract = False
-    fp1 = fplib_GD.get_fp(contract, ntyp, natx, lmax, lat1, rxyz1, types, znucl, cutoff)
-    fp2 = fplib_GD.get_fp(contract, ntyp, natx, lmax, lat2, rxyz2, types, znucl, cutoff)
+    fp1 = fplib_GD.get_fp(contract, ntyp, nx, lmax, lat1, rxyz1, types, znucl, cutoff)
+    fp2 = fplib_GD.get_fp(contract, ntyp, nx, lmax, lat2, rxyz2, types, znucl, cutoff)
 
     dist = fplib_GD.get_fpdist(ntyp, types, fp1, fp2)
     print ('fingerprint distance: ', dist)
@@ -45,4 +45,4 @@ if __name__ == "__main__":
     args = sys.argv
     v1 = args[1]
     v2 = args[2]
-    test(v1, v2)
+    test1(v1, v2)
