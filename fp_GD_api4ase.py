44f59f685e703a2654933b2650e36a5e69a10598
Tack_Tau <taoshuo950111@gmail.com>
2022-05-03 14:02:39 -0400

    Minor changes

--- fp_GD_api4ase.py
+++ fp_GD_api4ase.py
@@ -7,27 +7,87 @@ import numpy as np
 # import ase.units as units
 from ase.atoms import Atoms
 from ase.cell import Cell
-# from ase.calculators.genericfileio import (CalculatorTemplate,
-#                                            GenericFileIOCalculator)
-# from ase.calculators.calculator import BaseCalculator, FileIOCalculator
 from ase.calculators.calculator import Calculator
+from ase.calculators.calculator import CalculatorSetupError, all_changes
 
 
-class fp_GD_Calculator(object):
+class fp_GD_Calculator(Calculator):
     """Fingerprint calculator for ase"""
     
-    def __init__(self, parameters=None, atoms=None, **kwargs):
-        if parameters is None:
-            self.parameters = {}
-        else:
-            self.parameters = dict(parameters)
-        # self.parameters = dict(parameters)
-        self.atoms = {}
+    implemented_properties = [ 'energy', 'forces', 'stress' ]
+    
+    default_parameters = {
+                          'contract': False,
+                          'ntyp': 1,
+                          'nx': 300,
+                          'lmax': 0,
+                          'cutoff': 6.0,
+                          }
+    
+    def __init__(self, atoms=None, **kwargs):
+        self._atoms = None
         self.energy = None
         self.forces = None
-        # self.results = None
+        self.results = {}
+        # Initialize parameter dictionaries
+        self._store_param_state()  # Initialize an empty parameter state
+        
+        Calculator.__init__(self, atoms = atoms, **kwargs)
 
-    
+    def set(self, **kwargs):
+        """Override the set function, to test for changes in the
+        fingerprint Calculator.
+        """
+        changed_parameters = {}
+
+        if 'atoms' in kwargs:
+            atoms = kwargs.pop('atoms')
+            self.atoms = atoms  # Resets results
+
+        changed_parameters.update(Calculator.set(self, **kwargs))
+
+    def reset(self):
+        self.atoms = None
+        self.clear_results()
+
+    def clear_results(self):
+        self.results.clear()
+
+    def calculate(self,
+                  atoms = None,
+                  properties = [ 'energy', 'forces', 'stress' ],
+                  system_changes = tuple(all_changes),
+                 ):
+        """Do a fingerprint calculation in the specified directory.
+        This will read VASP input files (POSCAR) and then execute 
+        fp_GD.
+        """
+        # Check for zero-length lattice vectors and PBC
+        # and that we actually have an Atoms object.
+        # check_atoms(atoms)
+
+        # self.clear_results()
+        '''
+        if atoms is not None:
+            self.atoms = atoms.copy()
+        
+        if properties is None:
+            properties = self.implemented_properties
+        '''
+        Calculator.calculate(self, atoms, properties, system_changes)
+        if atoms is None:
+            atoms = self.atoms
+        # self.update_atoms(atoms)
+        
+        self.results['energy'] = self.get_potential_energy(atoms)
+        self.results['forces'] = self.get_forces(atoms)
+        self.results['stress'] = self.get_stress(atoms)
+        
+    def _store_param_state(self):
+        """Store current parameter state"""
+        self.param_state = dict(
+            default_parameters=self.default_parameters.copy()
+            )
     
     def check_restart(self, atoms=None, **kwargs):
         if (
@@ -50,7 +110,7 @@ class fp_GD_Calculator(object):
         # energy = self.results["energypotential"]["TOTAL"].energy * ENERGY_CONV["Hartree"]["eV"]
         # energy = self.results["density"].grid.mp.asum(energy)
         energy = fplib_GD.get_fp_energy(lat, rxyz, types, contract = False, ntyp = 1, nx = 100, \
-                                        lmax = 0, znucl = np.array([3], int), cutoff = 4.0)
+                                        lmax = 0, znucl = np.array([3], int), cutoff = 6.0)
         return energy
 
     def get_forces(self, atoms=None, **kwargs):
@@ -60,7 +120,7 @@ class fp_GD_Calculator(object):
             types = fplib_GD.read_types('Li-mp-51.vasp')
             self.get_potential_energy(atoms)
         forces = fplib_GD.get_fp_forces(lat, rxyz, types, contract = False, ntyp = 1, nx = 100, \
-                                        lmax = 0, znucl = np.array([3], int), cutoff = 4.0, \
+                                        lmax = 0, znucl = np.array([3], int), cutoff = 6.0, \
                                         iter_max = 1, step_size = 1e-4)
         return forces
 
@@ -72,8 +132,8 @@ class fp_GD_Calculator(object):
             pos = atoms.get_scaled_positions()
             types = fplib_GD.read_types('Li-mp-51.vasp')
             self.get_potential_energy(atoms)
-        stress = fplib_GD.get_FD_stress(lat, pos, types, contract = False, ntyp = 1, nx = 100, \
-                                        lmax = 0, znucl = np.array([3], int), cutoff = 4.0, \
-                                        iter_max = 1, step_size = 1e-4)
-        return stress
+        # stress = fplib_GD.get_FD_stress(lat, pos, types, contract = False, ntyp = 1, nx = 100, \
+        #                                 lmax = 0, znucl = np.array([3], int), cutoff = 6.0, \
+        #                                 iter_max = 1, step_size = 1e-4)
+        return np.zeros(6)
      
--- fplib_test/ase_test.py
+++ fplib_test/ase_test.py
@@ -3,9 +3,6 @@ import sys
 import numpy as np
 import fplib_GD
 import ase.io
-from ase import units
-# from ase.calculators.calculator import Calculator
-# from ase.lattice.cubic import FaceCenteredCubic
 from ase.optimize import BFGS, LBFGS, BFGSLineSearch, QuasiNewton, FIRE
 from ase.optimize.sciopt import SciPyFminBFGS, SciPyFminCG
 from ase.constraints import StrainFilter, UnitCellFilter
@@ -20,6 +17,8 @@ trajfile = 'opt.traj'
 calc = fp_GD_Calculator()
 # atoms.set_calculator(calc)
 atoms.calc = calc
+print ("fp_energy:\n", atoms.get_potential_energy())
+print ("fp_forces:\n", atoms.get_forces())
 
 ############################## Relaxation type ##############################
 '''
@@ -27,19 +26,20 @@ Ref :
     https://wiki.fysik.dtu.dk/ase/ase/optimize.html#module-optimize
     https://wiki.fysik.dtu.dk/ase/ase/constraints.html
 '''
+
 af = atoms
 # af = StrainFilter(atoms)
 # af = UnitCellFilter(atoms)
-print(af.get_forces())
+
 ############################## Relaxation method ##############################
-opt = BFGS(af, maxstep = 1.e-1, trajectory = trajfile)
-# opt = FIRE(af, maxstep = 1.e-1, trajectory = trajfile)
+# opt = BFGS(af, maxstep = 1.e-1, trajectory = trajfile)
+opt = FIRE(af, maxstep = 1.e-1, trajectory = trajfile)
 # opt = LBFGS(af, maxstep = 1.e-1, trajectory = trajfile, memory = 10, use_line_search = True)
 # opt = LBFGS(af, maxstep = 1.e-1, trajectory = trajfile, memory = 10, use_line_search = False)
 # opt = SciPyFminCG(af, trajectory = trajfile)
 # opt = SciPyFminBFGS(af, trajectory = trajfile)
 
-opt.run(fmax = 1.e-10)
+opt.run(fmax = 1.e-8)
 
 traj = Trajectory(trajfile)
 ase.io.write('opt.vasp', traj[-1], direct = True, long_format=True, vasp5 = True)
